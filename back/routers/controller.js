a06d6e3a85b71163bf5c2ff6698fec4c20f68ee2
ohdair <ohdiar@gmail.com>
2022-05-31 09:16:14 +0900

    #3 Add errorCode
    에러코드 입력, API마다 Error에 따른 Message 삽입

--- back/routers/controller.js
+++ back/routers/controller.js
@@ -2,22 +2,44 @@ const Friend42 = require("../models/useModel");
 
 const asyncHandler = require("express-async-handler");
 
+const errorCode = Object.freeze({
+  nonexistentUser: "Please enter a valid id", // 0
+  nonexistentId: "Please add a User Id", // 1
+  nonexistentIdAndList: "Please add a User Id or List Name", // 2
+  nonexistentFriend: "Please add a Friend", // 3
+  nonexistentMessage: "Please add a Message", // 4
+  existentValue: "is already exists", // 5
+});
+
+const errorPush = (res, errorNum, location) => {
+  var msg;
+  res.status(400);
+  if (errorNum === 0) msg = errorCode.nonexistentUser;
+  if (errorNum === 1) msg = errorCode.nonexistentId;
+  if (errorNum === 2) msg = errorCode.nonexistentIdAndList;
+  if (errorNum === 3) msg = errorCode.nonexistentFriend;
+  if (errorNum === 4) msg = errorCode.nonexistentMessage;
+  if (errorNum === 5) msg = `${location} ${errorCode.existentValue}`;
+  throw new Error(msg);
+};
+
 // @desc    Get User Info
 // @route   GET /api/user
 // @access  Private
+// Necessary factor : userId
 const findAll = asyncHandler(async (req, res) => {
-  const userInfo = await Friend42.findOne({ id: req.body.id });
-  res.send(userInfo);
+  if (!req.body.id) errorPush(res, 1);
+  const user = await Friend42.findOne({ id: req.body.id });
+  if (!user) errorPush(res, 0);
+  res.send(user);
 });
 
 // @desc    Create User
 // @route   POST /api/user
 // @access  Private
+// Necessary factor : userId
 const createUser = asyncHandler(async (req, res) => {
-  if (!req.body.id) {
-    res.status(400);
-    throw new Error("Please add a User Id");
-  }
+  if (!req.body.id) errorPush(res, 1);
   const user = await Friend42.create({
     id: req.body.id,
     name: req.body.name,
@@ -28,16 +50,14 @@ const createUser = asyncHandler(async (req, res) => {
 // @desc    Create List of friends
 // @route   POST /api/list
 // @access  Private
+// Necessary factor : userId, listName
 const createList = asyncHandler(async (req, res) => {
-  if (!req.body.id) {
-    res.status(400);
-    throw new Error("Please add a User Id");
-  }
-  if (!req.body.list) {
-    res.status(400);
-    throw new Error("Please add a List Name");
-  }
+  if (!req.body.id || !req.body.list) errorPush(res, 2);
   const user = await Friend42.findOne({ id: req.body.id });
+  if (!user) errorPush(res, 0);
+  user.friends.find((item) => {
+    if (item.listName === req.body.list) errorPush(res, 5, req.body.list);
+  });
   const newList = { listName: req.body.list };
   user.friends.push(newList);
   user.save();
@@ -47,14 +67,18 @@ const createList = asyncHandler(async (req, res) => {
 // @desc    Create friend
 // @route   POST /api/friend
 // @access  Private
+// Necessary factor : userId, friend
 const createFriend = asyncHandler(async (req, res) => {
-  if (!req.body.friend) {
-    res.status(400);
-    throw new Error("friend not found");
-  }
+  if (!req.body.id) errorPush(res, 1);
   const user = await Friend42.findOne({ id: req.body.id });
+  if (!user) errorPush(res, 0);
+  user.friends.map((list) => {
+    list.friends.find((friend) => {
+      if (friend === req.body.friend) errorPush(res, 5, req.body.friend);
+    });
+  });
   user.friends
-    .find((item) => item.listName === req.body.list)
+    .find((item) => item.listName === "친구")
     .friends.push(req.body.friend);
   user.save();
   res.status(200).json(user);
@@ -63,8 +87,11 @@ const createFriend = asyncHandler(async (req, res) => {
 // @desc    Add message
 // @route   POST /api/message
 // @access  Private
+// Necessary factor : userId
 const createMessage = asyncHandler(async (req, res) => {
+  if (!req.body.id) errorPush(res, 1);
   const user = await Friend42.findOne({ id: req.body.id });
+  if (!user) errorPush(res, 0);
   user.message.push(req.body.message);
   user.save();
   res.status(200).json(user);
@@ -73,8 +100,11 @@ const createMessage = asyncHandler(async (req, res) => {
 // @desc    Change list Name
 // @route   PUT /api/list
 // @access  Private
+// Necessary factor : userId, list, newList
 const updateList = asyncHandler(async (req, res) => {
+  if (!req.body.id || !req.body.list || !req.body.newList) errorPush(res, 2);
   const user = await Friend42.findOne({ id: req.body.id });
+  if (!user) errorPush(res, 0);
   user.friends.find((item) => {
     if (item.listName === req.body.list) item.listName = req.body.newList;
   });
@@ -85,8 +115,12 @@ const updateList = asyncHandler(async (req, res) => {
 // @desc    Move friend to another List
 // @route   PUT /api/friend
 // @access  Private
+// Necessary factor : userId, friend, list, newList
 const moveFriend = asyncHandler(async (req, res) => {
+  if (!req.body.id || !req.body.list || !req.body.newList) errorPush(res, 2);
+  if (!req.body.friend) errorPush(res, 3);
   const user = await Friend42.findOne({ id: req.body.id });
+  if (!user) errorPush(res, 0);
   user.friends
     .find((item) => item.listName === req.body.newList)
     .friends.push(req.body.friend);
@@ -100,8 +134,11 @@ const moveFriend = asyncHandler(async (req, res) => {
 // @desc    Delete list
 // @route   DELETE /api/list
 // @access  Private
+// Necessary factor : userId, list
 const deleteList = asyncHandler(async (req, res) => {
+  if (!req.body.id || !req.body.list) errorPush(res, 2);
   const user = await Friend42.findOne({ id: req.body.id });
+  if (!user) errorPush(res, 0);
   user.friends = user.friends.filter((item) => item.listName !== req.body.list);
   user.save();
   res.status(200).json(user);
@@ -110,11 +147,15 @@ const deleteList = asyncHandler(async (req, res) => {
 // @desc    Delete friend
 // @route   DELETE /api/friend
 // @access  Private
+// Necessary factor : userId, friend, list
 const deleteFriend = asyncHandler(async (req, res) => {
+  if (!req.body.id || !req.body.list) errorPush(res, 2);
+  if (!req.body.friend) errorPush(res, 3);
   const user = await Friend42.findOne({ id: req.body.id });
+  if (!user) errorPush(res, 0);
   const list = user.friends.find((item) => item.listName === req.body.list);
   list.friends = list.friends.filter((friend) => friend !== req.body.friend);
-  // user.save();
+  user.save();
   res.status(200).json(user);
 });
 
