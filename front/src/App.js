327a8e80623c3dab51dd2eba6332512e971fcc34
jungmyungjin <myung2667@gmail.com>
2022-06-03 19:08:39 +0900

    forntend React17 initialization

---
+++ front/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Button } from "semantic-ui-react";
+
+const ButtonExampleButton = () => <Button>Click Here</Button>;
+
+function App() {
+  return <div className="App"></div>;
+}
+
+export default App;
